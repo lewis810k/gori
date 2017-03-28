73b98fa55fd3975aa17f88b4131c3b014f494773
lewis810k <alsdl901@gmail.com>
2017-03-28 19:30:42 +0900

    user serializer 생성

---
+++ django_app/member/apis/user.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from member.serializers import UserSerializer
+
+
+class ProfileView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, format=None):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
--- django_app/talent/tests.py
+++ django_app/talent/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# 업로드된 이미지 경로 맞는지.
+
