780550402b8b88f4999dbe87f817a32d26803ced
Gosunghyun <gsh2448@gmail.com>
2017-03-30 13:18:49 +0900

    "admin 페이지 안 뜨는거 수정"

--- django_app/member/management/commands/createsu.py
+++ django_app/member/management/commands/createsu.py
@@ -9,13 +9,13 @@ User = get_user_model()
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print(config)
-        superuser_username = config['django']['superuser']['username']
+        # superuser_username = config['django']['superuser']['username']
         superuser_password = config['django']['superuser']['password']
         superuser_email = config['django']['superuser']['email']
 
-        if not User.objects.filter(username=superuser_username).exists():
+        if not User.objects.filter(email=superuser_email).exists():
             User.objects.create_superuser(
-                username=superuser_username,
+                # username=superuser_username,
                 password=superuser_password,
                 email=superuser_email
             )
