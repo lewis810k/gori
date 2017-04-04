f7cca6206622d35778e057c85e310be96741202c
lewis810k <alsdl901@gmail.com>
2017-04-04 13:49:54 +0900

    member app의 serializer 분리 작업

--- django_app/member/serializers/login.py
+++ django_app/member/serializers/login.py
@@ -1,70 +1,15 @@
 from allauth.account.adapter import get_adapter
 from allauth.account.utils import setup_user_email
 from allauth.socialaccount.helpers import complete_social_login
-from django.contrib.auth import get_user_model
 from rest_auth.registration.serializers import RegisterSerializer, SocialLoginSerializer
 from rest_framework import serializers
 from django.utils.translation import ugettext_lazy as _
-from member.models import Tutor
 from requests.exceptions import HTTPError
 
-User = get_user_model()
-
-
-class UserSerializer(serializers.ModelSerializer):
-    user_type = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = User
-        fields = (
-            'pk',
-            'username',
-            'name',
-            'user_type',
-            'is_staff',
-            'is_active',
-            'cellphone',
-            'profile_image',
-            'joined_date',
-            'is_tutor',
-            'last_login',
-        )
-
-    def get_user_type(self, obj):
-        return obj.get_user_type_display()
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username'],
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-
-
-class TutorSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(
-        read_only=True, source='user.username')
-    name = serializers.CharField(
-        read_only=True, source='user.name'
-    )
-    cellphone = serializers.CharField(
-        read_only=True, source='user.cellphone'
-    )
-    profile_image = serializers.ImageField(
-        read_only=True, source='user.profile_image')
-
-    class Meta:
-        model = Tutor
-        fields = (
-            'pk',
-            'username',
-            'name',
-            'user',
-            'is_verified',
-            'profile_image',
-            'cellphone',
-        )
+__all__ = (
+    'CustomLoginSerializer',
+    'CustomSocialLoginSerializer',
+)
 
 
 class CustomLoginSerializer(RegisterSerializer):
---
+++ django_app/member/serializers/user.py
@@ -0,0 +1,67 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from member.models import Tutor
+
+__all__ = (
+    'UserSerializer',
+    'TutorSerializer',
+)
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    user_type = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'pk',
+            'username',
+            'name',
+            'user_type',
+            'is_staff',
+            'is_active',
+            'cellphone',
+            'profile_image',
+            'joined_date',
+            'is_tutor',
+            'last_login',
+        )
+
+    def get_user_type(self, obj):
+        return obj.get_user_type_display()
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class TutorSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        read_only=True, source='user.username')
+    name = serializers.CharField(
+        read_only=True, source='user.name'
+    )
+    cellphone = serializers.CharField(
+        read_only=True, source='user.cellphone'
+    )
+    profile_image = serializers.ImageField(
+        read_only=True, source='user.profile_image')
+
+    class Meta:
+        model = Tutor
+        fields = (
+            'pk',
+            'username',
+            'name',
+            'user',
+            'is_verified',
+            'profile_image',
+            'cellphone',
+        )
