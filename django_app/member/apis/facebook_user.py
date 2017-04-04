c7713cac36a2285f57e5d98b568229c3a3a14247
lewis810k <alsdl901@gmail.com>
2017-04-04 15:22:08 +0900

    facebook custom login에서 프로필 이미지 불러와서 모델에 저장하는 부분 구현

---
+++ django_app/member/apis/facebook_user.py
@@ -0,0 +1,75 @@
+from urllib.request import urlopen
+
+import requests
+from allauth.socialaccount.providers.facebook.provider import FacebookProvider, GRAPH_API_URL
+from allauth.socialaccount.providers.facebook.views import compute_appsecret_proof
+from allauth.socialaccount.providers.oauth2.views import OAuth2Adapter
+from django.core.files import File
+from django.core.files.temp import NamedTemporaryFile
+from rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount import app_settings, providers
+
+from member.serializers import CustomSocialLoginSerializer
+
+
+def fb_profile_image_save(login):
+    """
+    페이스북 유저정보에서 유저id를 받아온다.
+    유저id를 이용하여 프로필 사진을 불러온다.
+        프로필 사진을 불러올 때 사이즈를 크게 한다.
+    유저 정보에 이미지를 저장 한다.
+    :return: login
+    """
+    image_url = 'https://graph.facebook.com/' + login.user.username + '/picture?type=large&width=200&height=200'
+    img_temp = NamedTemporaryFile(delete=True)
+    img_temp.write(urlopen(image_url).read())
+    img_temp.flush()
+    login.user.profile_image.save('image_{}.{}'.format(login.user.username, 'jpg'), File(img_temp))
+    return login
+
+
+def fb_complete_login(request, app, token):
+    provider = providers.registry.by_id(FacebookProvider.id, request)
+    resp = requests.get(
+        GRAPH_API_URL + '/me',
+        params={
+            'fields': ','.join(provider.get_fields()),
+            'access_token': token.token,
+            'appsecret_proof': compute_appsecret_proof(app, token)
+        })
+
+    resp.raise_for_status()
+    extra_data = resp.json()
+
+    # 커스터마이징 시작
+    # user 모델의 username 필드를 unique하게 저장하기 때문에 id값으로 변경한다.
+    extra_data['username'] = extra_data['id']
+    login = provider.sociallogin_from_response(request, extra_data)
+    # soiciallogin을 통해서 채워진 데이터 이외에 추가정보를 입력한다.
+    # name은 기존에 name을 이용하여 처리.
+    login.user.name = extra_data['name']
+    # facebook유저는 f로 구분
+    login.user.user_type = 'f'
+    # 프로필 이미지 받아오기
+    login = fb_profile_image_save(login)
+
+    return login
+
+
+class FacebookOAuth2TempAdapter(OAuth2Adapter):
+    provider_id = FacebookProvider.id
+    provider_default_auth_url = 'https://www.facebook.com/dialog/oauth'
+
+    settings = app_settings.PROVIDERS.get(provider_id, {})
+
+    authorize_url = settings.get('AUTHORIZE_URL', provider_default_auth_url)
+    access_token_url = GRAPH_API_URL + '/oauth/access_token'
+    expires_in_key = 'expires_in'
+
+    def complete_login(self, request, app, access_token, **kwargs):
+        return fb_complete_login(request, app, access_token)
+
+
+class CreateFacebookUserView(SocialLoginView):
+    adapter_class = FacebookOAuth2TempAdapter
+    serializer_class = CustomSocialLoginSerializer
--- django_app/member/apis/user.py
+++ django_app/member/apis/user.py
@@ -1,20 +1,11 @@
-import requests
-from allauth.account.adapter import get_adapter
-from allauth.socialaccount.providers.facebook.provider import FacebookProvider, GRAPH_API_URL
-from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter, compute_appsecret_proof
-from allauth.socialaccount.providers.oauth2.views import OAuth2Adapter
 from django.contrib.auth import get_user_model
-from rest_auth.registration.serializers import SocialLoginSerializer
-from rest_auth.registration.views import RegisterView, SocialLoginView
-from rest_auth.views import LoginView
+from rest_auth.registration.views import RegisterView
 from rest_framework import generics
 from rest_framework import permissions
-from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from allauth.socialaccount import app_settings, providers
 
-from member.serializers import UserSerializer, CustomLoginSerializer, CustomSocialLoginSerializer
+from member.serializers import UserSerializer, CustomLoginSerializer
 
 User = get_user_model()
 
@@ -54,49 +45,3 @@ class DestroyUserView(generics.RetrieveUpdateDestroyAPIView):
 class CreateDjangoUserView(RegisterView):
     serializer_class = CustomLoginSerializer
 
-
-def fb_complete_login(request, app, token):
-    provider = providers.registry.by_id(FacebookProvider.id, request)
-    resp = requests.get(
-        GRAPH_API_URL + '/me',
-        params={
-            'fields': ','.join(provider.get_fields()),
-            'access_token': token.token,
-            'appsecret_proof': compute_appsecret_proof(app, token)
-        })
-
-    resp.raise_for_status()
-
-    extra_data = resp.json()
-    print(extra_data)
-    print('===========')
-    extra_data['username'] = extra_data['id']
-    # request.data['user_type'] = 'f'
-    login = provider.sociallogin_from_response(request, extra_data)
-    print(login.user)
-    login.user.name = extra_data['name']
-    login.user.user_type = 'f'
-    # 이거는.. 모든 생성 과정 끝나면 이미지 따로 업로드 하는 것으로!!!!
-    # login.user.profile_image = 'https://graph.facebook.com/' + login.user.username + '/picture'
-    print('---------------')
-    print(dir(login.user))
-    return login
-
-
-class FacebookOAuth2TempAdapter(OAuth2Adapter):
-    provider_id = FacebookProvider.id
-    provider_default_auth_url = 'https://www.facebook.com/dialog/oauth'
-
-    settings = app_settings.PROVIDERS.get(provider_id, {})
-
-    authorize_url = settings.get('AUTHORIZE_URL', provider_default_auth_url)
-    access_token_url = GRAPH_API_URL + '/oauth/access_token'
-    expires_in_key = 'expires_in'
-
-    def complete_login(self, request, app, access_token, **kwargs):
-        return fb_complete_login(request, app, access_token)
-
-
-class CreateFacebookUserView(SocialLoginView):
-    adapter_class = FacebookOAuth2TempAdapter
-    serializer_class = CustomSocialLoginSerializer
