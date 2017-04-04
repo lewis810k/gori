8472c925657b205c33881bf5c6156c2e91fdab81
youngnac <youngnac@gmail.com>
2017-04-05 01:25:08 +0900

    Registration의 student와 talent_location에 related_name 추가

--- django_app/talent/models/registration.py
+++ django_app/talent/models/registration.py
@@ -14,13 +14,18 @@ class Registration(models.Model):
         (2, '초/중급자'),
         (3, '상급자'),
     )
-    student = models.ForeignKey(settings.AUTH_USER_MODEL, )
-    talent_location = models.ForeignKey(Location, )
+    student = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                related_name='registrations')
+    talent_location = models.ForeignKey(Location,
+                                        related_name='registrations')
     joined_date = models.DateTimeField(auto_now_add=True)
     is_confirmed = models.BooleanField(default=False)
-    student_level = models.IntegerField(choices=LEVEL, help_text="레벨 선택", default=1)
-    experience_length = models.IntegerField(default=0, blank=True, help_text="해당 수업관련 경력을 개월로 입력")
-    message_to_tutor = models.CharField(max_length=50, help_text="수강신청시 유저가 튜터에게 보내는 메세지", blank=False)
+    student_level = models.IntegerField(choices=LEVEL,
+                                        help_text="레벨 선택", default=1)
+    experience_length = models.IntegerField(default=0, blank=True,
+                                            help_text="해당 수업관련 경력을 개월로 입력")
+    message_to_tutor = models.CharField(max_length=50,
+                                        help_text="수강신청시 유저가 튜터에게 보내는 메세지", blank=False)
 
     def __str__(self):
         return '{} 님  {}: {} 수업을 신청하였습니다'.format(self.student.username, self.talent_location.talent.pk,
