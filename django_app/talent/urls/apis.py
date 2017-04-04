8cd88189d819de32b2fcf020d142fcd2dd094ce6
youngnac <youngnac@gmail.com>
2017-04-04 17:16:23 +0900

    api url 수정

--- django_app/talent/urls/apis.py
+++ django_app/talent/urls/apis.py
@@ -4,9 +4,12 @@ from .. import apis
 
 urlpatterns = [
     url(r'^list/$', apis.TalentList.as_view()),
-    url(r'^list/location/(?P<pk>[0-9]+)/$', apis.LocationRetrieve.as_view()),
+    url(r'^detail/location/(?P<pk>[0-9]+)/$', apis.LocationRetrieve.as_view()),
+    url(r'^detail/classimage/(?P<pk>[0-9]+)/$', apis.ClassImageRetrieve.as_view()),
+    url(r'^detail/curriculum/(?P<pk>[0-9]+)/$', apis.CurriculumRetrieve.as_view()),
     url(r'^wishlist/$', apis.WishList.as_view()),
-    url(r'^detail/(?P<pk>[0-9]+)/$', apis.TalentDetail.as_view()),
+    url(r'^detail/(?P<pk>[0-9]+)/$', apis.TalentShortDetail.as_view()),
+    url(r'^detail-all/(?P<pk>[0-9]+)/$', apis.TalentDetail.as_view()),
     url(r'^list/location/$', apis.LocationList.as_view()),
     url(r'^registration/$', apis.RegistrationList.as_view()),
     url(r'^(?P<pk>[0-9]+)/registration/$', apis.TalentRegistration.as_view()),
