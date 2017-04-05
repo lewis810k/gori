b477be2db80320ea001487c4301aa83524f85139
youngnac <youngnac@gmail.com>
2017-04-05 12:17:31 +0900

    tutor serializer dynamic field model serializer 상속

--- django_app/member/serializers.py
+++ django_app/member/serializers.py
@@ -43,7 +43,28 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 
-class TutorSerializer(serializers.ModelSerializer):
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that takes an additional `fields` argument that
+    controls which fields should be displayed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Don't pass the 'fields' arg up to the superclass
+        fields = kwargs.pop('fields', None)
+
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+
+        if fields:
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            existing = set(self.fields.keys())
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class TutorSerializer(DynamicFieldsModelSerializer):
     username = serializers.CharField(
         read_only=True, source='user.username')
     name = serializers.CharField(
