cfb001559f66dc410acb4754c4630620fd9f9cdf
lewis810k <alsdl901@gmail.com>
2017-04-05 21:40:02 +0900

    list pagination을 limit 방식으로 설정

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -101,6 +101,8 @@ AUTHENTICATION_BACKENDS = (
 )
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
