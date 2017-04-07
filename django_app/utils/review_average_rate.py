568d2c76e5d43b14481ca86daa444c60ef77f610
youngnac <youngnac@gmail.com>
2017-04-07 18:26:00 +0900

    review avg rate util 추가

---
+++ django_app/utils/review_average_rate.py
@@ -0,0 +1,14 @@
+
+def review_average_rate(reviews):
+    sum_of_average = 0
+    if reviews.count() > 0:
+        for review in reviews.all():
+            sum_of_average += review.average_rate
+        try:
+            return_value = sum_of_average / reviews.count()
+        except ZeroDivisionError:
+            return 0
+
+        return round(return_value, 1)
+    else:
+        return 0
