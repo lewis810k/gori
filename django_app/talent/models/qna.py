bfd434e7744c03aaa4112fa635809362f32a391a
Gosunghyun <gsh2448@gmail.com>
2017-04-10 16:13:55 +0900

    www.mozzi.co.kr 도 들어 가게끔 수정

---
+++ django_app/talent/models/qna.py
@@ -0,0 +1,13 @@
+# from django.contrib.auth import get_user_model
+# from django.db import models
+#
+# from member.models import Tutor
+# from talent.models import Talent
+#
+# User = get_user_model()
+#
+# class Qna(models.Model):
+#     talent = models.ForeignKey(Talent)
+#     user = models.ForeignKey(User)
+#     question = models.CharField(max_length=200)
+#     answer = models.CharField(max_length=200)
