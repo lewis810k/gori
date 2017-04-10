da9aa58cbf0efd7d7ddc19d29a7aeb843f6f0f67
Gosunghyun <gsh2448@gmail.com>
2017-04-10 14:26:05 +0900

    front corsheaders사용 가능하게 setting.py 변경

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -156,13 +156,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
-# # CORS_ORIGIN_WHITELIST = (
-# #     'localhost:8000',
-# # )
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080',
+)
 
 ROOT_URLCONF = 'config.urls'
 
