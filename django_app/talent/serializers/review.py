515728e338f45dd3f19f487557c2ef77deb581d0
youngnac <youngnac@gmail.com>
2017-04-11 14:20:56 +0900

    review serializer에 각각의 평점을 average_rates로 추가

--- django_app/talent/serializers/review.py
+++ django_app/talent/serializers/review.py
@@ -4,7 +4,8 @@ from rest_framework import serializers
 from member.serializers.user import ReviewUserSerializer
 from talent.models import Review
 from talent.models import Talent
-from utils import review_average_rate
+from utils import review_average_rate, curriculum_average_rate, timeliness_average_rate, delivery_average_rate, \
+    friendliness_average_rate, readiness_average_rate
 
 __all__ = (
     'ReviewSerializer',
@@ -33,11 +34,56 @@ class ReviewSerializer(serializers.ModelSerializer):
         )
 
 
+class AverageRatesSerializer(serializers.ModelSerializer):
+    total = serializers.SerializerMethodField(read_only=True)
+    curriculum = serializers.SerializerMethodField(read_only=True)
+    timeliness = serializers.SerializerMethodField(read_only=True)
+    readiness = serializers.SerializerMethodField(read_only=True)
+    delivery = serializers.SerializerMethodField(read_only=True)
+    friendliness = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Talent
+        fields = (
+            'total',
+            'curriculum',
+            'readiness',
+            'timeliness',
+            'delivery',
+            'friendliness',
+
+        )
+
+    @staticmethod
+    def get_total(obj):
+        return review_average_rate(obj.reviews)
+
+    @staticmethod
+    def get_curriculum(obj):
+        return curriculum_average_rate(obj.reviews)
+
+    @staticmethod
+    def get_timeliness(obj):
+        return timeliness_average_rate(obj.reviews)
+
+    @staticmethod
+    def get_friendliness(obj):
+        return friendliness_average_rate(obj.reviews)
+
+    @staticmethod
+    def get_delivery(obj):
+        return delivery_average_rate(obj.reviews)
+
+    @staticmethod
+    def get_readiness(obj):
+        return readiness_average_rate(obj.reviews)
+
+
 class ReviewWrapperSerializer(serializers.ModelSerializer):
     category = serializers.SerializerMethodField(read_only=True)
     type = serializers.SerializerMethodField(read_only=True)
     reviews = ReviewSerializer(many=True)
-    average_rate = serializers.SerializerMethodField(read_only=True)
+    average_rates = serializers.SerializerMethodField(read_only=True)
     review_count = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
@@ -47,7 +93,7 @@ class ReviewWrapperSerializer(serializers.ModelSerializer):
             'title',
             'category',
             'type',
-            'average_rate',
+            'average_rates',
             'review_count',
             'reviews',
         )
@@ -61,8 +107,8 @@ class ReviewWrapperSerializer(serializers.ModelSerializer):
         return obj.get_type_display()
 
     @staticmethod
-    def get_average_rate(obj):
-        return review_average_rate(obj.reviews)
+    def get_average_rates(obj):
+        return AverageRatesSerializer(obj).data
 
     @staticmethod
     def get_review_count(obj):
