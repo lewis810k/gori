651a0f57bcf4d1f5b1482973b42e2a826675f25f
youngnac <youngnac@gmail.com>
2017-04-14 00:34:27 +0900

    인증승인 대기 수업 보여주는 api와 sales 상태 toggle api 구현

--- django_app/talent/apis/talent.py
+++ django_app/talent/apis/talent.py
@@ -4,18 +4,21 @@ from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from talent.models import Talent
 from talent.serializers import TalentDetailSerializer
-from talent.serializers import TalentListSerializer, TalentShortDetailSerializer
-from utils import duplicate_verify, Tutor
+from talent.serializers import TalentListSerializer, TalentShortDetailSerializer, TalentShortInfoSerializer
+from utils import duplicate_verify, Tutor, custom_permission, switch_sales_status
 
 __all__ = (
     'TalentListCreateView',
+    'UnverifiedTalentListView',
     # detail - all
     'TalentDetailView',
     # detail - fragments
     'TalentShortDetailView',
+    'TalentSalesStatusToggleView',
 )
 
 User = get_user_model()
@@ -121,8 +124,15 @@ class TalentListCreateView(generics.ListCreateAPIView):
             queryset = queryset.filter(locations__region__icontains=region).distinct('pk')
         if category is not None:
             queryset = queryset.filter(category__icontains=category)
-
+        queryset = queryset.filter(is_verified=True)
         return queryset
+
+
+# 인증 승인이 필요한 talent list api
+class UnverifiedTalentListView(generics.ListAPIView):
+    permission_classes = (custom_permission.CustomerIsAdminAccessPermission,)
+    queryset = Talent.objects.filter(is_verified=False)
+    serializer_class = TalentListSerializer
 
 
 class TalentShortDetailView(generics.RetrieveAPIView):
@@ -134,3 +144,21 @@ class TalentShortDetailView(generics.RetrieveAPIView):
 class TalentDetailView(generics.RetrieveAPIView):
     queryset = Talent.objects.all()
     serializer_class = TalentDetailSerializer
+
+
+# talent의 is_soldout 상태 toggle
+class TalentSalesStatusToggleView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, pk):
+        tutor = request.user.tutor
+        try:
+            talent = Talent.objects.get(pk=pk)
+            if talent in tutor.talent_set.all():
+                talent, detail = switch_sales_status(pk)
+                return Response(status=status.HTTP_200_OK,
+                                data={"detail": detail, "result": TalentShortInfoSerializer(talent).data})
+            else:
+                return Response(status=status.HTTP_401_UNAUTHORIZED, data={"detail": "해당 요청에 대한 권한이 없습니다."})
+        except Talent.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"detail": "해당 수 내역을 찾을 수 없습니다."})
--- django_app/talent/urls/apis.py
+++ django_app/talent/urls/apis.py
@@ -14,13 +14,14 @@ urlpatterns = [
     url(r'^add/class-image/$', apis.ClassImageListCreateView.as_view()),
     url(r'^add/location/$', apis.LocationCreateView.as_view(),name='location-create'),
     url(r'^add/curriculum/$', apis.CurriculumListCreateView.as_view()),
-    url(r'^add/registration/$', apis.RegistrationListCreateView.as_view()),
+    url(r'^add/registration/$', apis.RegistrationListCreateView.as_view(),name='registration-create'),
     url(r'^add/review/$', apis.ReviewCreateView.as_view()),
     url(r'^add/question/$', apis.QuestionCreateView.as_view()),
     url(r'^add/reply/$', apis.ReplyCreateView.as_view()),
 
     # ##### 리스트 #####
     url(r'^list/$', apis.TalentListCreateView.as_view(), name='list'),
+    url(r'^list/unverified/$', apis.UnverifiedTalentListView.as_view(), name='list-unverified'),
 
     # ##### 전체보기 #####
     url(r'^detail-all/(?P<pk>[0-9]+)/$', apis.TalentDetailView.as_view(), name='detail-all'),
@@ -30,11 +31,12 @@ urlpatterns = [
     url(r'^detail/(?P<pk>[0-9]+)/location/$', apis.LocationRetrieveView.as_view(),name='location-retrieve'),
     url(r'^detail/(?P<pk>[0-9]+)/class-image/$', apis.ClassImageRetrieveView.as_view()),
     url(r'^detail/(?P<pk>[0-9]+)/curriculum/$', apis.CurriculumRetrieveView.as_view()),
-    url(r'^detail/(?P<pk>[0-9]+)/registration/$', apis.TalentRegistrationRetrieveView.as_view()),
+    url(r'^detail/(?P<pk>[0-9]+)/registration/$', apis.TalentRegistrationRetrieveView.as_view(),name='registration-retrieve'),
     url(r'^detail/(?P<pk>[0-9]+)/review/$', apis.ReviewRetrieveView.as_view()),
     url(r'^detail/(?P<pk>[0-9]+)/qna/$', apis.QnATalentRetrieveView.as_view()),
 
     # ##### 위시리스트 추가/삭제 #####
     url(r'^(?P<pk>[0-9]+)/wish-list/toggle/$', m_apis.WishListToggleView.as_view(), name='wishlist-toggle'),
+    url(r'^(?P<pk>[0-9]+)/sales-status/toggle/$', apis.TalentSalesStatusToggleView.as_view(), name='wishlist-toggle'),
     # url(r'^registration/$', apis.RegistrationList.as_view()),
 ]
