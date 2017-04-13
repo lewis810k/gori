ae2dd7e1b00554a8e2d2482493fcd147c05cb300
lewis810k <alsdl901@gmail.com>
2017-04-13 11:33:41 +0900

    pagination limit-offset에서 page_number로 변경

--- django_app/utils/pagination.py
+++ django_app/utils/pagination.py
@@ -1,4 +1,4 @@
-from rest_framework.pagination import CursorPagination
+from rest_framework.pagination import CursorPagination, LimitOffsetPagination, PageNumberPagination
 
 __all__ = (
     'TalentPagination',
@@ -6,9 +6,15 @@ __all__ = (
 )
 
 
-class TalentPagination(CursorPagination):
+class TalentPagination(LimitOffsetPagination):
     page_size = 2
     ordering = 'created_date'
+
+
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 1000
+    page_size_query_param = 'page_size'
+    max_page_size = 10000
 
 
 class RegistrationPagination(CursorPagination):
