f4a5d7d496b025ddc2f548fa01055d97e239c037
youngnac <youngnac@gmail.com>
2017-04-14 12:17:36 +0900

    registration create test

--- django_app/talent/tests.py
+++ django_app/talent/tests.py
@@ -4,14 +4,14 @@ from rest_framework.authtoken.models import Token
 from rest_framework.reverse import reverse
 from rest_framework.test import APILiveServerTestCase
 
-from talent.models import Location, Registration
+from talent.models import Registration, Location
 from utils import APITest_User_Login
 
 User = get_user_model()
-#
+
+
 # class GoriTest(APILiveServerTestCase, APITest_User_Login):
 #     def test_token_create(self):
-#
 #         response = self.client.post(url, HTTP_AUTHORIZATION='Token ' + token.data.get('token'))
 #
 #     def test_talent_create(self):
@@ -19,6 +19,7 @@ User = get_user_model()
 #         data = {
 #
 #         }
+#
 #     def test_talent_list(self):
 #         """
 #         params : resgion:SD 은 사당으로 검색하기에
@@ -45,23 +46,21 @@ User = get_user_model()
 #         response = self.client.get(url, params)
 #         print(response.data)
 #         self.assertEqual(len(response.data), 0)
-#          self.assertEqual(response.status_code, status.HTTP_200_OK)
-#     def test_url_exist(self):
-#         talent = self.create_talent()
-#         talent_list_url = reverse('api:talent:talent-list')
-#         talent_detail_url = reverse('api:talent:talent-detail')
-#         talent_detail_all_url = reverse('api:talent:talent-detail-all')
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
 #
-#         talent_list = self.client.get(talent_list_url)
-#         talent_detail = self.client.get(talent_detail_url)
-#         talent_detail_all = self.client.get(talent_detail_all_url)
 #
-#         self.assertEqual(talent_list.status_code, status.HTTP_200_OK)
-#         self.assertEqual(talent_detail_all.status_code, status.HTTP_200_OK)
-#         self.assertEqual(talent_detail.status_code, status.HTTP_200_OK)
+# def test_url_exist(self):
+#     talent = self.create_talent()
+#     talent_list_url = reverse('api:talent:talent-list')
+#     talent_detail_url = reverse('api:talent:talent-detail')
+#     talent_detail_all_url = reverse('api:talent:talent-detail-all')
+#     talent_list = self.client.get(talent_list_url)
+#     talent_detail = self.client.get(talent_detail_url)
+#     talent_detail_all = self.client.get(talent_detail_all_url)
+#     self.assertEqual(talent_list.status_code, status.HTTP_200_OK)
+#     self.assertEqual(talent_detail_all.status_code, status.HTTP_200_OK)
+#     self.assertEqual(talent_detail.status_code, status.HTTP_200_OK)
 #
-
-
 
 class LocationCreateRetriveTest(APILiveServerTestCase, APITest_User_Login):
     def test_location_create(self):
@@ -115,7 +114,53 @@ class LocationCreateRetriveTest(APILiveServerTestCase, APITest_User_Login):
             response = self.client.get(location_retrieve_url)
             self.assertEqual(response.status_code, expected_status_code)
             if response.status_code == status.HTTP_200_OK:
-                self.assertEqual(response.data["pk"],talent.pk)
-                self.assertIn("locations",response.data)
-                self.assertIn("region",response.data["locations"][0])
-                self.assertIn("day",response.data["locations"][0])
+                self.assertEqual(response.data["pk"], talent.pk)
+                self.assertIn("locations", response.data)
+                self.assertIn("region", response.data["locations"][0])
+                self.assertIn("day", response.data["locations"][0])
+
+
+class RegistrationCreateRetrieveTest(APILiveServerTestCase, APITest_User_Login):
+    def test_registration_create(self):
+        users, tokens = self.obtain_token(3)
+        tutor = self.create_tutor(users[0], tokens[0])
+        talent = self.create_talent(tutor, tokens[0])
+        location = self.create_location(talent, tokens[0])
+        message_to_tutor = "잘부탁드립니다"
+        data = {
+            "location_pk": location.pk,
+            "student_level": 1,
+            "message_to_tutor": message_to_tutor,
+        }
+        url = reverse('api:talent:registration-create')
+        test_tokens = [(tokens[2] + '###', status.HTTP_401_UNAUTHORIZED),
+                       ('', status.HTTP_401_UNAUTHORIZED),
+                       (tokens[0], status.HTTP_400_BAD_REQUEST),
+                       (tokens[1], status.HTTP_201_CREATED),
+                       (tokens[1], status.HTTP_400_BAD_REQUEST), ]
+        for token, expected_status_code in test_tokens:
+            response = self.client.post(url, data, HTTP_AUTHORIZATION='Token ' + token)
+            self.assertEqual(response.status_code, expected_status_code)
+            if response.status_code == status.HTTP_201_CREATED:
+                self.assertEqual(Registration.objects.get(student=users[1]).message_to_tutor, message_to_tutor)
+                self.assertEqual(Registration.objects.filter(student=users[1]).count(), 1)
+                self.assertEqual(Registration.objects.filter(talent_location=location).count(), 1)
+
+    def test_registration_retrieve(self):
+        users, tokens = self.obtain_token(4)
+        tutor = self.create_tutor(users[0], tokens[0])
+        talent = self.create_talent(tutor, tokens[0])
+        location = self.create_location(talent, tokens[0])
+        for index, token in enumerate(tokens[1:]):
+            self.create_registration(location=location, token=token)
+            registration_retrieve_url = reverse('api:talent:registration-retrieve', kwargs={'pk': talent.pk})
+            response = self.client.get(registration_retrieve_url, HTTP_AUTHORIZATION='Token ' + token)
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertIn(str(Token.objects.get(key=token).user.pk),
+                             response.data["registrations"][index]["message_to_tutor"])
+            self.assertEqual(talent.title, response.data["title"])
+        invalid_talent_pk = talent.pk + 1
+        registration_retrieve_url = reverse('api:talent:registration-retrieve', kwargs={'pk': invalid_talent_pk})
+        response = self.client.get(registration_retrieve_url, HTTP_AUTHORIZATION='Token ' + tokens[1])
+        self.assertEqual(status.HTTP_400_BAD_REQUEST,response.status_code)
+
