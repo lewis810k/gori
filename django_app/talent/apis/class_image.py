680249a5523ac67e67a27d03791fc4327df624ff
lewis810k <alsdl901@gmail.com>
2017-04-15 17:04:00 +0900

    class image create 리팩토링

--- django_app/talent/apis/class_image.py
+++ django_app/talent/apis/class_image.py
@@ -5,8 +5,8 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from talent.models import Talent, ClassImage
-from talent.serializers import ClassImageWrapperSerializer, ClassImageSerializer
-from utils import verify_tutor, LargeResultsSetPagination
+from talent.serializers import ClassImageSerializer, ClassImageCreateSerializer
+from utils import *
 
 __all__ = (
     'ClassImageListCreateView',
@@ -22,44 +22,28 @@ class ClassImageListCreateView(generics.ListCreateAPIView):
     def get_queryset(self):
         return ClassImage.objects.filter(talent_id=self.kwargs['pk'])
 
-    def post(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         """
-
         필수정보 :
             - talent_pk : 수업 아이디
             - image : 커리큘럼 이미지
         """
+        # 생성 전용 시리얼라이저 사용
+        serializer = ClassImageCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-        try:
-            talent_pk = request.data['talent_pk']
-            talent = Talent.objects.filter(pk=talent_pk).first()
-            if not talent:
-                ret = {
-                    'detail': '수업({pk})이 존재하지 않습니다.'.format(pk=talent_pk)
-                }
-                return Response(ret, status=status.HTTP_400_BAD_REQUEST)
+        # ##### 추가 검증 절차 #####
+        talent = Talent.objects.get(pk=request.data['talent_pk'])
 
-            if verify_tutor(request, talent):
-                ClassImage.objects.create(
-                    talent=talent,
-                    image=request.FILES['image'],
-                )
-                ret_message = '[{talent}]에 [{image}]가 추가되었습니다.'.format(
-                    talent=talent.title,
-                    image=request.FILES['image'],
-                )
-                ret = {
-                    'detail': ret_message,
-                }
-                return Response(ret, status=status.HTTP_201_CREATED)
+        # ##### 자신의 수업이 아니면 등록 불가능 #####
+        if not verify_tutor(request, talent):
+            return Response(authorization_error, status=status.HTTP_400_BAD_REQUEST)
 
-            ret = {
-                'detail': '권한이 없습니다.',
-            }
-            return Response(ret, status=status.HTTP_401_UNAUTHORIZED)
+        # ##### 갯수 제한? #####
 
-        except MultiValueDictKeyError as e:
-            ret = {
-                'non_field_error': (str(e)).strip('"') + ' field가 제공되지 않았습니다.'
-            }
-            return Response(ret, status=status.HTTP_400_BAD_REQUEST)
+        # ##### 추가 검증 끝  #####
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(success_msg, status=status.HTTP_201_CREATED, headers=headers)
--- django_app/talent/serializers/class_image.py
+++ django_app/talent/serializers/class_image.py
@@ -4,7 +4,7 @@ from talent.models import ClassImage, Talent
 
 __all__ = (
     'ClassImageSerializer',
-    'ClassImageWrapperSerializer',
+    'ClassImageCreateSerializer',
 )
 
 
@@ -20,25 +20,12 @@ class ClassImageSerializer(serializers.ModelSerializer):
         )
 
 
-class ClassImageWrapperSerializer(serializers.ModelSerializer):
-    category = serializers.SerializerMethodField(read_only=True)
-    type = serializers.SerializerMethodField(read_only=True)
-    class_images = ClassImageSerializer(many=True, source='classimage_set')
+class ClassImageCreateSerializer(serializers.ModelSerializer):
+    talent_pk = serializers.PrimaryKeyRelatedField(queryset=Talent.objects.all(), source='talent')
 
     class Meta:
-        model = Talent
+        model = ClassImage
         fields = (
-            'pk',
-            'title',
-            'category',
-            'type',
-            'class_images',
+            'talent_pk',
+            'image',
         )
-
-    @staticmethod
-    def get_category(obj):
-        return obj.get_category_display()
-
-    @staticmethod
-    def get_type(obj):
-        return obj.get_type_display()
