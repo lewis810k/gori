1fe418d738770156ae3106994f465fadc3a36983
lewis810k <alsdl901@gmail.com>
2017-04-15 17:09:17 +0900

    curriculum create 리팩토링

--- django_app/talent/apis/curriculum.py
+++ django_app/talent/apis/curriculum.py
@@ -4,8 +4,9 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from talent.models import Curriculum, Talent
-from talent.serializers import CurriculumSerializer, CurriculumWrapperSerializer
-from utils import verify_tutor, LargeResultsSetPagination
+from talent.serializers import CurriculumSerializer
+from talent.serializers.curriculum import CurriculumCreateSerializer
+from utils import *
 
 __all__ = (
     'CurriculumListCreateView',
@@ -20,46 +21,30 @@ class CurriculumListCreateView(generics.ListCreateAPIView):
     def get_queryset(self):
         return Curriculum.objects.filter(talent_id=self.kwargs['pk'])
 
-    def post(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         """
-
         필수정보 :
             - talent_pk : 수업 아이디
             - information : 커리큘럼 설명
         추가정보 :
             - image : 커리큘럼 이미지
         """
-        try:
-            talent_pk = request.data['talent_pk']
-            talent = Talent.objects.filter(pk=talent_pk).first()
-            if not talent:
-                ret = {
-                    'detail': '수업({pk})이 존재하지 않습니다.'.format(pk=talent_pk)
-                }
-                return Response(ret, status=status.HTTP_400_BAD_REQUEST)
+        # 생성 전용 시리얼라이저 사용
+        serializer = CurriculumCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-            if verify_tutor(request, talent):
-                item, _ = Curriculum.objects.get_or_create(
-                    talent=talent,
-                    information=request.data['information'],
-                    image=request.FILES.get('image', ''),
-                )
-                ret_message = '[{talent}]에 수업이 추가되었습니다.'.format(
-                    talent=talent.title,
-                    information=request.data['information']
-                )
-                ret = {
-                    'detail': ret_message,
-                }
-                return Response(ret, status=status.HTTP_201_CREATED)
+        # ##### 추가 검증 절차 #####
+        talent = Talent.objects.get(pk=request.data['talent_pk'])
 
-            ret = {
-                'detail': '권한이 없습니다.',
-            }
-            return Response(ret, status=status.HTTP_401_UNAUTHORIZED)
+        # ##### 자신의 수업이 아니면 등록 불가능 #####
+        if not verify_tutor(request, talent):
+            return Response(authorization_error, status=status.HTTP_400_BAD_REQUEST)
 
-        except MultiValueDictKeyError as e:
-            ret = {
-                'non_field_error': (str(e)).strip('"') + ' field가 제공되지 않았습니다.'
-            }
-            return Response(ret, status=status.HTTP_400_BAD_REQUEST)
+        # ##### 갯수 제한? #####
+
+        # ##### 추가 검증 끝  #####
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(success_msg, status=status.HTTP_201_CREATED, headers=headers)
